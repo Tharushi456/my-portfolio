94db4dd4afe495d4dfcc60163d547e97f9b491dd
Tharushi456 <dimalshacooray@gmail.com>
2025-07-23 13:49:57 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>Hi, I'm Tharushi</h1>
+      <p>Application Management Intern | React Developer</p>
+    </header>
+  );
+}
+export default Header;
